3f05880adcfff453181203d41dbf848d62b48feb
João Vitor Fröhlich <joaovitorfrohlich@gmail.com>
2022-09-18 00:21:03 -0300

    Update script.py

--- moedas/script.py
+++ moedas/script.py
@@ -1,5 +1,19 @@
 from Imagem import *
 
-img = Imagem('moeda.jpeg')
-img.limiar(0,230)
-img.vizinhanca()
+img = Imagem('foto2.jpg')
+# img.salvar_imagem('cinza.jpg')
+img.limiar(0,160)
+# img.salvar_imagem('limiar.jpg')
+conjuntos = list(reversed(sorted(img.vizinhanca())))
+
+base = conjuntos[0]
+to_erase = []
+
+for conjunto in conjuntos:
+    if conjunto/base < 0.01:
+        to_erase.append(conjunto)
+
+for conjunto in to_erase:
+    conjuntos.remove(conjunto)
+
+print(conjuntos)
