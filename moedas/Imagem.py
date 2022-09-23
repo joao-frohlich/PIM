6434d15aaefcae91acf86f1dcf04ef3d3a4dfa77
Joao Vitor Frohlich <joao.frohlich@edu.udesc.br>
2022-09-23 10:41:30 -0300

    Trocando DSU por uma BFS

--- moedas/Imagem.py
+++ moedas/Imagem.py
@@ -1,11 +1,12 @@
 import cv2
-from DSU import *
+from queue import Queue
 
 class Imagem:
     def __init__(self, caminho):
         self.imagem = cv2.imread(caminho, 2)
         self.altura = len(self.imagem)
         self.largura = len(self.imagem[0])
+        self.tam_conjuntos = []
     
     def salvar_imagem(self, nome):
         cv2.imwrite(nome, self.imagem)
@@ -20,22 +21,29 @@ class Imagem:
                     self.imagem[i][j] = 0
     
     def valido(self, x, y):
-        return (x >= 0 and x <= self.altura and y >= 0 and y <= self.largura)
+        return (x >= 0 and x < self.altura and y >= 0 and y < self.largura)
 
-    def vizinhanca(self):
+    def pegar_objetos(self):
         pixels = []
         dicio = {}
+        visitados = []
+        for i in range(self.altura):
+            visitados.append([False]*self.largura)
         for i in range(self.altura):
             for j in range(self.largura):
-                if self.imagem[i][j] == 255:
-                    dicio[(i,j)] = len(pixels)
-                    pixels.append((i,j))
-        conjuntos = DSU(len(pixels))
-        for (a,b) in pixels:
-            if self.valido(a-1,b):                
-                if self.imagem[a-1][b] == 255:
-                    conjuntos.union(dicio[(a,b)], dicio[(a-1,b)])
-            if self.valido(a,b-1):
-                if self.imagem[a][b-1] == 255:
-                    conjuntos.union(dicio[(a,b)], dicio[(a,b-1)])
-        return conjuntos.tam_conjuntos()
+                if self.imagem[i][j] == 255 and not visitados[i][j]:
+                    q = Queue()
+                    q.put((i,j))
+                    sz = 0
+                    while not q.empty():
+                        (pi, pj) = q.get()
+                        if visitados[pi][pj]:
+                            continue
+                        visitados[pi][pj] = True
+                        sz += 1
+                        mvs = [(1,0), (-1,0), (0,1), (0,-1)]
+                        for mv in mvs:
+                            if self.valido(pi+mv[0], pj+mv[1]) and self.imagem[pi+mv[0]][pj+mv[1]] == 255:
+                                q.put((pi+mv[0], pj+mv[1]))
+                    self.tam_conjuntos.append(sz)
+        return self.tam_conjuntos
--- moedas/script.py
+++ moedas/script.py
@@ -1,19 +1,36 @@
 from Imagem import *
 
 img = Imagem('foto2.jpg')
-# img.salvar_imagem('cinza.jpg')
+img.salvar_imagem('cinza.jpg')
 img.limiar(0,160)
-# img.salvar_imagem('limiar.jpg')
-conjuntos = list(reversed(sorted(img.vizinhanca())))
+img.salvar_imagem('limiar.jpg')
+tam_conjuntos = img.pegar_objetos()
+print(tam_conjuntos)
+tam_ord = list(reversed(sorted(tam_conjuntos)))
+print(tam_ord)
 
-base = conjuntos[0]
+base = tam_ord[0]
 to_erase = []
 
-for conjunto in conjuntos:
+for conjunto in tam_ord:
     if conjunto/base < 0.01:
         to_erase.append(conjunto)
 
 for conjunto in to_erase:
-    conjuntos.remove(conjunto)
+    tam_conjuntos.remove(conjunto)
+    tam_ord.remove(conjunto)
 
-print(conjuntos)
+print(tam_conjuntos)
+print(tam_ord)
+
+moedas = [1, 0]
+moeda = 0
+
+for i in range(1,len(tam_ord)):
+    if tam_ord[i]/tam_ord[i-1] < 0.8:
+        moeda += 1
+    if moeda > 1: break
+    moedas[moeda]+=1
+    
+
+print('R$ ' + f'{moedas[0] + 0.1*moedas[1]:.2f}')
